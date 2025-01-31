b2b631e4105094cce2ccc3eee36a4b00cfcf758a
Lukehsu <lukehsu1999@gmail.com>
2025-01-31 14:48:43 +0800

    SwipeCard: allow video display

--- src/SwipeCard.js
+++ src/SwipeCard.js
@@ -9,24 +9,25 @@ const SwipeableCard = () => {
   const [rightSwipes, setRightSwipes] = useState(0);
 
   const cards = [
-    { name: "Samurai & Ninja Museum", img: "/card_pictures/Kyoto/Samurai_ninja_museum_1.jpg" },
-    { name: "Samurai & Ninja Museum", img: "/card_pictures/Kyoto/Samurai_ninja_museum_2.jpeg" },
-    { name: "Nijo Castle", img: "/card_pictures/Kyoto/Nijo_castle_1.jpg" },
-    { name: "Nijo Castle", img: "/card_pictures/Kyoto/Nijo_castle_2.jpg" },
-    { name: "Gion", img: "/card_pictures/Kyoto/Gion_1.jpg" },
-    { name: "Gion", img: "/card_pictures/Kyoto/Gion_2.jpg" },
-    { name: "Gion", img: "/card_pictures/Kyoto/Gion_3.jpg" },
-    { name: "Fushimi Inaria Taisha Shrine", img: "/card_pictures/Kyoto/Fushimi_Inari_Taisha_Shrine_1.jpg" },
-    { name: "Fushimi Inaria Taisha Shrine", img: "/card_pictures/Kyoto/Fushimi_Inari_Taisha_Shrine_2.jpg" },
-    { name: "Fushimi Inaria Taisha Shrine", img: "/card_pictures/Kyoto/Fushimi_Inari_Taisha_Shrine_3.jpg"},
-    { name: "Kinkakuji Temple", img: "/card_pictures/Kyoto/Kinkakuji_Temple_1.jpg"},
-    { name: "Kinkakuji Temple", img: "/card_pictures/Kyoto/Kinkakuji_Temple_2.jpg"},
-    { name: "Kinkakuji Temple", img: "/card_pictures/Kyoto/Kinkakuji_Temple_3.jpg"},
-    { name: "Kiyomizu-dera Temple", img: "/card_pictures/Kyoto/Kiyomizu_dera_Temple_1.jpg"},
-    { name: "Kiyomizu-dera Temple", img: "/card_pictures/Kyoto/Kiyomizu_dera_Temple_2.jpg"},
-    { name :"Arashiyama", img: "/card_pictures/Kyoto/Arashiyama_1.jpg"},
-    { name :"Arashiyama", img: "/card_pictures/Kyoto/Arashiyama_2.jpg"},
-    { name :"Arashiyama", img: "/card_pictures/Kyoto/Arashiyama_3.jpg"}
+    { name: "Philosopher's Walk", type: "video", video: "/card_pictures/Kyoto/Philosophers_walk_4.mp4" },
+    { name: "Samurai & Ninja Museum", type: "image", img: "/card_pictures/Kyoto/Samurai_ninja_museum_1.jpg" },
+    { name: "Samurai & Ninja Museum", type: "image", img: "/card_pictures/Kyoto/Samurai_ninja_museum_2.jpeg" },
+    { name: "Nijo Castle", type: "image", img: "/card_pictures/Kyoto/Nijo_castle_1.jpg" },
+    { name: "Nijo Castle", type: "image", img: "/card_pictures/Kyoto/Nijo_castle_2.jpg" },
+    { name: "Gion", type: "image", img: "/card_pictures/Kyoto/Gion_1.jpg" },
+    { name: "Gion", type: "image", img: "/card_pictures/Kyoto/Gion_2.jpg" },
+    { name: "Gion", type: "image", img: "/card_pictures/Kyoto/Gion_3.jpg" },
+    { name: "Fushimi Inaria Taisha Shrine", type: "image", img: "/card_pictures/Kyoto/Fushimi_Inari_Taisha_Shrine_1.jpg" },
+    { name: "Fushimi Inaria Taisha Shrine", type: "image", img: "/card_pictures/Kyoto/Fushimi_Inari_Taisha_Shrine_2.jpg" },
+    { name: "Fushimi Inaria Taisha Shrine", type: "image", img: "/card_pictures/Kyoto/Fushimi_Inari_Taisha_Shrine_3.jpg"},
+    { name: "Kinkakuji Temple", type: "image", img: "/card_pictures/Kyoto/Kinkakuji_Temple_1.jpg"},
+    { name: "Kinkakuji Temple", type: "image", img: "/card_pictures/Kyoto/Kinkakuji_Temple_2.jpg"},
+    { name: "Kinkakuji Temple", type: "image", img: "/card_pictures/Kyoto/Kinkakuji_Temple_3.jpg"},
+    { name: "Kiyomizu-dera Temple", type: "image", img: "/card_pictures/Kyoto/Kiyomizu_dera_Temple_1.jpg"},
+    { name: "Kiyomizu-dera Temple", type: "image", img: "/card_pictures/Kyoto/Kiyomizu_dera_Temple_2.jpg"},
+    { name :"Arashiyama", type: "image", img: "/card_pictures/Kyoto/Arashiyama_1.jpg"},
+    { name :"Arashiyama", type: "image", img: "/card_pictures/Kyoto/Arashiyama_2.jpg"},
+    { name :"Arashiyama", type: "image", img: "/card_pictures/Kyoto/Arashiyama_3.jpg"}
   ];
 
   const handleSwipe = (direction) => {
@@ -56,7 +57,14 @@ const SwipeableCard = () => {
     <div {...handlers} className="card-container">
       {index < cards.length ? (
         <div className={`card ${swipeDirection}`} key={cards[index].name}>
-          <img src={cards[index].img} alt={cards[index].name} />
+          {cards[index].type === "video" ? (
+            <video controls width="100%" autoPlay loop>
+              <source src={cards[index].video} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+          ) : (
+            <img src={cards[index].img} alt={cards[index].name} />
+          )}
           <h3>{cards[index].name}</h3>
         </div>
       ) : (
