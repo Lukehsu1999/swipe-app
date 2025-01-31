845cc828ddf9eb47272c2504107fae5fbaed9388
Lukehsu <lukehsu1999@gmail.com>
2025-01-31 20:01:09 +0800

    refactor: move card Data separately

---
+++ frontend/src/cardData.js
@@ -0,0 +1,23 @@
+const allCards = [
+    { name: "Philosopher's Walk", type: "video", video: "/card_pictures/Kyoto/Philosophers_walk_4.mp4" },
+    { name: "Samurai & Ninja Museum", type: "image", img: "/card_pictures/Kyoto/Samurai_ninja_museum_1.jpg" },
+    { name: "Samurai & Ninja Museum", type: "image", img: "/card_pictures/Kyoto/Samurai_ninja_museum_2.jpeg" },
+    { name: "Nijo Castle", type: "image", img: "/card_pictures/Kyoto/Nijo_castle_1.jpg" },
+    { name: "Nijo Castle", type: "image", img: "/card_pictures/Kyoto/Nijo_castle_2.jpg" },
+    { name: "Gion", type: "image", img: "/card_pictures/Kyoto/Gion_1.jpg" },
+    { name: "Gion", type: "image", img: "/card_pictures/Kyoto/Gion_2.jpg" },
+    { name: "Gion", type: "image", img: "/card_pictures/Kyoto/Gion_3.jpg" },
+    { name: "Fushimi Inaria Taisha Shrine", type: "image", img: "/card_pictures/Kyoto/Fushimi_Inari_Taisha_Shrine_1.jpg" },
+    { name: "Fushimi Inaria Taisha Shrine", type: "image", img: "/card_pictures/Kyoto/Fushimi_Inari_Taisha_Shrine_2.jpg" },
+    { name: "Fushimi Inaria Taisha Shrine", type: "image", img: "/card_pictures/Kyoto/Fushimi_Inari_Taisha_Shrine_3.jpg"},
+    { name: "Kinkakuji Temple", type: "image", img: "/card_pictures/Kyoto/Kinkakuji_Temple_1.jpg"},
+    { name: "Kinkakuji Temple", type: "image", img: "/card_pictures/Kyoto/Kinkakuji_Temple_2.jpg"},
+    { name: "Kinkakuji Temple", type: "image", img: "/card_pictures/Kyoto/Kinkakuji_Temple_3.jpg"},
+    { name: "Kiyomizu-dera Temple", type: "image", img: "/card_pictures/Kyoto/Kiyomizu_dera_Temple_1.jpg"},
+    { name: "Kiyomizu-dera Temple", type: "image", img: "/card_pictures/Kyoto/Kiyomizu_dera_Temple_2.jpg"},
+    { name :"Arashiyama", type: "image", img: "/card_pictures/Kyoto/Arashiyama_1.jpg"},
+    { name :"Arashiyama", type: "image", img: "/card_pictures/Kyoto/Arashiyama_2.jpg"},
+    { name :"Arashiyama", type: "image", img: "/card_pictures/Kyoto/Arashiyama_3.jpg"}
+];
+
+export default allCards;
